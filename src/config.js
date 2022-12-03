6f8ec8fb6861da447af22858709bbb5ba45d74c8
Aman Raj <aman@spottabl.com>
2022-12-03 12:18:51 +0530

    new server

--- src/config.js
+++ src/config.js
@@ -1,6 +1,10 @@
 // const API_URL = "http://localhost:3000/api";
 // const API_URL = "https://dev.amantradingcompany.in/api";
-const API_URL = "https://atc-backend.herokuapp.com/api";
+// const API_URL = "https://atc-backend.herokuapp.com/api";
 // const API_URL = "http://ec2-13-232-175-204.ap-south-1.compute.amazonaws.com:3000/api";
+const API_URL = "https://atc-backend.onrender.com/api";
+
+
+
 
 module.exports = API_URL;
