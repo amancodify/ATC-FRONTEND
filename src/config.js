d2562edf8e4cef305b71660a696eabb7a2ee45ae
Aman Raj <aman@spottabl.com>
2025-07-10 08:07:27 +0530

    Update API_URL in config.js to point to the new Koyeb deployment

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // const API_URL = "http://localhost:8000/api";
 // const API_URL = "https://dev.amantradingcompany.in/api";
-// const API_URL = "https://atc-backend.herokuapp.com/api";
-// const API_URL = "http://ec2-13-232-175-204.ap-south-1.compute.amazonaws.com:3000/api";
-const API_URL = "https://atc-backend.onrender.com/api";
+// const API_URL = "https://atc-backend.onrender.com/api";
+const API_URL = "https://deliberate-cynde-amantradingcompany-cb2aa72f.koyeb.app/api";
 
 module.exports = API_URL;
+
