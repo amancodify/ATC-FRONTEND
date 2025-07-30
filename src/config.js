51940c1e74d35456a45430bc7bb791cddd1bdb48
Aman Raj <aman@spottabl.com>
2025-07-30 09:24:07 +0530

    Update API_URL in config.js to point to the local development server

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
-// const API_URL = "http://localhost:8000/api";
+const API_URL = "http://localhost:8000/api";
 // const API_URL = "https://dev.amantradingcompany.in/api";
 // const API_URL = "https://atc-backend.onrender.com/api";
-const API_URL = "https://deliberate-cynde-amantradingcompany-cb2aa72f.koyeb.app/api";
 
 module.exports = API_URL;
 
