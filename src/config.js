f47dad46e4528743563328f6e92717dcd84e5f0d
Aman Raj <aman@spottabl.com>
2025-07-30 13:48:57 +0530

    Uncomment local API_URL and ensure correct API_URL is set for production

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-const API_URL = "http://localhost:8000/api";
+// const API_URL = "http://localhost:8000/api";
 // const API_URL = "https://dev.amantradingcompany.in/api";
-// const API_URL = "https://atc-backend.onrender.com/api";
+const API_URL = "https://atc-backend.onrender.com/api";
 
 module.exports = API_URL;
 
